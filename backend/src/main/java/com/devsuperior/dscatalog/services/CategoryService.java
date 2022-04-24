91be5b5bdbeaadf5c664c000c9b6965aa7819050
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-04-23 22:56:05 -0300

    Dto

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,12 +1,14 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 
@@ -17,8 +19,11 @@ public class CategoryService {
 	private CategoryRepository categoryRepository;
 	
 	@Transactional
-	public List<Category> findAll() {
-		return categoryRepository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list =  categoryRepository.findAll();
+		List<CategoryDTO> listDto = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		//List<CategoryDTO> listDto = list.stream().map(x -> new CategoryDTO(x)).toList();
+		return listDto;
 	}
 	
 }
