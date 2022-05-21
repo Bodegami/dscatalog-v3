30c44f8f52e08c79acdc809c5289adcb3e23722b
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-05-21 18:00:52 -0300

    add ProductService test deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -1,20 +1,17 @@
 package com.devsuperior.dscatalog.services;
 
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.devsuperior.dscatalog.repositories.ProductRepository;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -34,9 +31,19 @@ public class ProductServiceTests {
 		existingId = 1L;
 		nonExistingId = 1000L;
 		
-		doNothing().when(repository).deleteById(existingId);
+		Mockito.doNothing().when(repository).deleteById(existingId);
 		
-		doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
+		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
+	}
+	
+	@Test
+	public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+		
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.delete(nonExistingId);
+		});
+		
+		Mockito.verify(repository, Mockito.times(1)).deleteById(nonExistingId);
 	}
 	
 	@Test
@@ -48,7 +55,8 @@ public class ProductServiceTests {
 		
 		//Mockito.verify(repository, Mockito.times(2)).deleteById(existingId); -> verifica qtas vezes foi chamado
 		//Mockito.verify(repository, Mockito.never()).deleteById(existingId); -> verifica se nunca foi chamado
-		verify(repository, times(1)).deleteById(existingId);
+		Mockito.verify(repository, Mockito.times(1)).deleteById(existingId);
 	}
 	
+
 }
